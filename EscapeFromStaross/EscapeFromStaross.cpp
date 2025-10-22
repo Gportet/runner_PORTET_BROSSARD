2ee2c5b34e233afca1524fb1ecc5feb1909247a9
Gaël <gael.portet@gmail.com>
2025-10-22 09:41:24 +0200

    Fusion branche Player sur Master

--- EscapeFromStaross/EscapeFromStaross.cpp
+++ EscapeFromStaross/EscapeFromStaross.cpp
@@ -21,7 +21,7 @@ int main()
     window.setFramerateLimit(60);
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
